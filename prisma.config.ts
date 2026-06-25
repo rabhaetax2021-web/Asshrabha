7ab4c8872c33bc30ccc9485a94e28adc1b0e6077
aboelmagdzaid <rabhaetax2021@gmail.com>
2026-06-25 20:24:03 +0300

    6

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
-  datasource: {
-    url: process.env.DATABASE_URL || 'postgresql://postgres:24636243@localhost:2463/asshrabha?schema=public',
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations'
   },
+  datasource: {
+    url: env('DATABASE_URL')
+  }
 })
